f6029f68f64aacf56ce4ff2df73e5effee3de2c3
jasoncc95 <jasoncc95@gmail.com>
2024-07-09 19:37:37 -0600

    Adding board state, alternating between X and O

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { useState } from "react";
 import Square from "./Square";
 //The export JavaScript keyword makes this function accessible outside of this file. 
 //The default keyword tells other files using your code that it’s the main function in your file.
 export default function Board() {
+  //by declaring this variables with useState we can maintain and change the state of the board
+  const [xIsNext, setXIsNext] = useState(true); //to alternate between X and O
+  const [squares, setSquares] = useState(Array(9).fill(null)); //creates an array with nine elements and sets each of them to null
+  
+  function handleClick(i) {
+    if (squares[i]) return; //if there is already a value, do nothing
+
+    const nextSquares = squares.slice();
+    xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
+    setSquares(nextSquares); //setting the next state of the board
+    setXIsNext(!xIsNext); //setting which letter is next
+  }
+  
+  //each Square is a component and we pass down the props it requires: the value to display 
+  //and a callback function to keep the state of the game
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={ squares[0] } onSquareClick={ () => handleClick(0) }/>
+        <Square value={ squares[1] } onSquareClick={ () => handleClick(1) }/>
+        <Square value={ squares[2] } onSquareClick={ () => handleClick(2) }/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={ squares[3] } onSquareClick={ () => handleClick(3) }/>
+        <Square value={ squares[4] } onSquareClick={ () => handleClick(4) }/>
+        <Square value={ squares[5] } onSquareClick={ () => handleClick(5) }/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={ squares[6] } onSquareClick={ () => handleClick(6) }/>
+        <Square value={ squares[7] } onSquareClick={ () => handleClick(7) }/>
+        <Square value={ squares[8] } onSquareClick={ () => handleClick(8) }/>
       </div>
     </>
   );
--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 //had to add export default for other files to be able to import it
-export default function Square() {
-    const [value, setValue] = useState(null);
+//each parameter passed when creating a Square is prop that is set on creation
+export default function Square({ value, onSquareClick }) {
+    //const [value, setValue] = useState(null);
+    //function handleClick() {
+    //    setValue("X")
+    //}
+    //return <button className="square" onClick={handleClick}>{value}</button>;
 
-    function handleClick() {
-        setValue("X")
-    }
-    
-    return <button className="square" onClick={handleClick}>{value}</button>;
+    //onSquareClick is a callback function that will get executed when the square is clicked
+    return <button className="square" onClick={ onSquareClick }>{ value }</button>;
 }
