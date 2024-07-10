714c342fa84abe62da904bccf1348040c002c10e
jasoncc95 <jasoncc95@gmail.com>
2024-07-09 20:56:39 -0600

    Calculating the winner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Square from "./Square";
+import { calculateWinner } from "./WinnerCalculator";
 //The export JavaScript keyword makes this function accessible outside of this file. 
 //The default keyword tells other files using your code that it’s the main function in your file.
 export default function Board() {
@@ -8,7 +9,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null)); //creates an array with nine elements and sets each of them to null
   
   function handleClick(i) {
-    if (squares[i]) return; //if there is already a value, do nothing
+    if (calculateWinner(squares) || squares[i]) return; //if there is already a winner or a value, do nothing
 
     const nextSquares = squares.slice();
     xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
@@ -16,10 +17,18 @@ export default function Board() {
     setXIsNext(!xIsNext); //setting which letter is next
   }
   
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = winner == "Draw" ? "Its a draw" : 'Winner is ' + winner;
+  } else {
+    status = 'Next player is ' + (xIsNext ? 'X' : 'O');
+  }
   //each Square is a component and we pass down the props it requires: the value to display 
   //and a callback function to keep the state of the game
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={ squares[0] } onSquareClick={ () => handleClick(0) }/>
         <Square value={ squares[1] } onSquareClick={ () => handleClick(1) }/>
